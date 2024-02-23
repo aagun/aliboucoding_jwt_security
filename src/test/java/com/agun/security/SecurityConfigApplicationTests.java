07e58918724ce1ab9821b41d9f43fdc30c155d13
= <agun@duck.com>
2024-02-23 17:39:13 +0700

    Initial project

---
+++ src/test/java/com/agun/security/SecurityConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agun.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
